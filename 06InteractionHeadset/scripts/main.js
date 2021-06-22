099e0cbbe14e06c6f70e2e6211e87afccaaaf991
yoanadotina <59655769+yoanadotina@users.noreply.github.com>
2021-06-22 14:08:45 +0300

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -6,7 +6,7 @@ function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
+	console.log(myOtherBox.object3D.rotation);
 }
 setInterval(spin, 10);
 
@@ -28,16 +28,16 @@ myOtherBox.addEventListener('mouseleave', function(){
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time. 
  */
 
-// var growspeed = 1.2;
+var growspeed = 1.2;
 
-// function grow(){
-// 	myOtherBox.object3D.scale.x *= growspeed;
-// 	myOtherBox.object3D.scale.y *= growspeed;
-// 	myOtherBox.object3D.scale.z *= growspeed;
-// 	//console.log(myOtherBox.object3D.scale);
-// }
+function grow(){
+myOtherBox.object3D.scale.x *= growspeed;
+myOtherBox.object3D.scale.y *= growspeed;
+myOtherBox.object3D.scale.z *= growspeed;
+console.log(myOtherBox.object3D.scale);
+}
 
-// myOtherBox.addEventListener('click', function(){ // uses a fuse
-// 	grow();
-// 	console.log('grew');
-// });
+myOtherBox.addEventListener('click', function(){ // uses a fuse
+grow();
+console.log('grew');
+});
